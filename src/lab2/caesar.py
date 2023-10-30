1cc5440f0180ba98a1e8b76fcee3f8209c6ebf28
Никита Тюняев <nikitatunaev@POCO-X3-Pro-2.local>
2023-10-30 23:25:55 +0300

    some code

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,26 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if (ord(i) >= 65 and ord(i) <= 90) or (ord(i) >= 97 and ord(i) <= 122):
+            diff = ord(i) + shift
+            if i.islower(): #lowercase
+                if diff > 122:
+                    diff -= 122
+                    diff += 97 - 1
+            else:
+                if diff > 90:
+                    diff -= 90
+                    diff += 90 -1
+            ciphertext += chr(diff)
+        else:
+            ciphertext += i 
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +42,21 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if (ord(i) >= 65 and ord(i) <= 90) or (ord(i) >= 97 and ord(i) <= 122):
+            diff = ord(i) - shift
+            if i.islower():
+                if diff < 97:
+                    diff -= 97
+                    diff += 122 + 1
+            else:
+                if diff < 65:
+                    diff -= 65
+                    diff += 90 + 1
+            plaintext += chr(diff)
+        else:
+             plaintext += i
     return plaintext
+print(decrypt_caesar('Sbwkrq3.6'))
+
+    
--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,31 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        if 97 <= ord(plaintext[i]) <= 122 \
+           or 65 <= ord(plaintext[i]) <= 90:
+            if plaintext[i].islower():
+                diff = ord(keyword[i % len(keyword)]) - 97
+                different = ord(plaintext[i]) + diff
+                if different > 122:
+                    different -= 122
+                    different += 97 - 1
+            else:
+                diff = ord(keyword[i % len(keyword)]) - 65
+                different = ord(plaintext[i]) + diff
+                if different > 90:
+                    different -= 90
+                    different += 65 - 1
+            ciphertext += chr(different)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
-
+print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +43,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        if 97 <= ord(ciphertext[i]) <= 122 \
+           or 65 <= ord(ciphertext[i]) <= 90:
+            if ciphertext[i].islower():
+                diff = ord(keyword[i % len(keyword)]) - 97
+                different = ord(ciphertext[i]) - diff
+                if different < 97:
+                    different -= 97
+                    different += 122 + 1
+            else:
+                diff = ord(keyword[i % len(keyword)]) - 65
+                different = ord(ciphertext[i]) - diff
+                if different < 65:
+                    different -= 65
+                    different += 90 + 1
+            plaintext += chr(different)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
+print(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"))
